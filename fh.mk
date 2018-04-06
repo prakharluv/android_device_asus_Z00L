24ec491d159b838ec1ba0e4828f6facbb7557dae
prakharluv <prakhar.dc@gmail.com>
2018-04-06 07:59:59 +0000

    Z00l: Fire up!

---
+++ fh.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/asus/Z00L/full_Z00L.mk)
+
+# Inherit some common FH stuff.
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Z00L
+PRODUCT_NAME := fh_Z00L
+PRODUCT_BRAND := asus
+PRODUCT_MANUFACTURER := asus
+DISABLE_CNM := True
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME="ZenFone 2 Laser"
